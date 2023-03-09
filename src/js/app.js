1d90169b19fdacc3697292b30a12f3e55a1f4010
Luis Carrillo <luis@d3v.mx>
2023-03-09 14:34:29 -0600

    v2.0.1

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,30 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelectorAll('.sidebar .list-group-item').forEach(function(element){
+      
+      element.addEventListener('click', function (e) {
+  
+        let nextEl = element.nextElementSibling;
+        let parentEl  = element.parentElement;	
+  
+          if(nextEl) {
+              e.preventDefault();	
+              let mycollapse = new bootstrap.Collapse(nextEl);
+              
+              if(nextEl.classList.contains('show')){
+                mycollapse.hide();
+              } else {
+                  mycollapse.show();
+                  // find other submenus with class=show
+                  var opened_submenu = parentEl.parentElement.querySelector('.submenu.show');
+                  // if it exists, then close all of them
+                  if(opened_submenu){
+                    new bootstrap.Collapse(opened_submenu);
+                  }
+              }
+          }
+      }); // addEventListener
+    }) // forEach
+  }); 
 function onLoading(o) {
     if (o) {
         document.getElementById('progress').style.display = 'flex';
