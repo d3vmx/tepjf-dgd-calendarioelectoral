758eaf9a34cb8890b5e40424fc7c7ac6dd9d2a57
Luis Carrillo <luis@d3v.mx>
2023-03-08 18:15:07 -0600

    init

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+function onLoading(o) {
+    if (o) {
+        document.getElementById('progress').style.display = 'flex';
+    }
+    else {
+        document.getElementById('progress').style.display = 'none';
+    }
+}
